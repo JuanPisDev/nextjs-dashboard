ece87a1ec98012aa881e8992749aeb46dfa9b740
Juan Pablo <juanpadev2003@gmail.com>
2026-05-25 13:36:57 -0500

    Idk deleting {Status} again

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,7 @@
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-import postgres {State} from 'postgres';
+import postgres from 'postgres';
 import {signIn} from '@/auth';
 import { AuthError } from 'next-auth';
 
@@ -16,7 +16,8 @@ const FormSchema = z.object({
     customerId: z.string({
         invalid_type_error: 'Please select a customer',
     }),
-    amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
+    amount: z.coerce.number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
     status: z.enum(['pending', 'paid'], {
         invalid_type_error: 'Please select an invoice status',
     }),
